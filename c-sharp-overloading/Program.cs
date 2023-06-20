3ef5aaa239452c26858f99b107832609e0fbfd19
omranali2111 <aljabri1234@hotmail.com>
2023-06-20 12:56:51 +0400

    method overloading

---
+++ c-sharp-overloading/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+       // Console.WriteLine("enter firs num");
+       //double x=double.Parse(Console.ReadLine());
+       // Console.WriteLine("enter second num");
+       // double y = double.Parse(Console.ReadLine());
+
+        Console.WriteLine( add(2, 3));
+        Console.WriteLine(add(2.5, 3.2,2.3));
+        Console.WriteLine(add(2, 3, 2));
+
+    }
+
+
+    static int add(int x, int y)
+    {
+        return x + y;
+    }
+    static int add(int x, int y,int z)
+    {
+        return x + y + z;
+    }
+
+    static double add(double x, double y, double z)
+    {
+        return x + y + z;
+    }
+}
